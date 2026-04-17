2f8c1aeacbcb34ef278630fc64cf71c8e9c5000d
arancissimo22-afk <arancissimo22@gmail.com>
2026-04-17 13:15:42 +0930

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,11 @@
-{children} 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
